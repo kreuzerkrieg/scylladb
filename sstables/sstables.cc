86ef3838aec9c7a006e33cd4b26be2fab2aebcf5
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-03-26 16:30:46 +0200

    sstables: replace `make_file_input_stream` with `make_data_or_index_source` where appropriate

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -2085,8 +2085,8 @@ future<> sstable::generate_summary() {
         auto s = summary_generator(_schema->get_partitioner(), _components->summary, _manager.config().sstable_summary_ratio());
             auto ctx = make_lw_shared<index_consume_entry_context<summary_generator>>(
                     *this, sem.make_tracking_only_permit(_schema, "generate-summary", db::no_timeout, {}), s, trust_promoted_index::yes,
-                    make_file_input_stream(index_file, 0, index_size, std::move(options)), 0, index_size,
-                    get_column_translation(*_schema), _manager._abort);
+                    input_stream<char>(_storage->make_data_or_index_source(*this, component_type::Index, 0, index_size, std::move(options))),
+                    0, index_size, get_column_translation(*_schema), _manager._abort);
 
         try {
             co_await ctx->consume_input();
@@ -2478,7 +2478,7 @@ input_stream<char> sstable::data_stream(uint64_t pos, size_t len,
                 *checksum, pos, len, std::move(options), digest, error_handler);
         }
     }
-    return make_file_input_stream(f, pos, len, std::move(options));
+    return input_stream<char>(_storage->make_data_or_index_source(*this, component_type::Data, pos, len, std::move(options)));
 }
 
 future<temporary_buffer<char>> sstable::data_read(uint64_t pos, size_t len, reader_permit permit) {
@@ -3467,6 +3467,9 @@ std::vector<std::unique_ptr<sstable_stream_source>> create_stream_sources(const 
                     }
                 };
                 co_return input_stream<char>(data_source(std::make_unique<buffer_data_source_impl>(std::move(bufs))));
+            }
+            if (_type == component_type::Data || _type == component_type::Index) {
+                co_return input_stream<char>(_sst->get_storage().make_data_or_index_source(*_sst, _type, 0, std::numeric_limits<uint64_t>::max(), options));
             }
             co_return make_file_input_stream(_file, options);
         }
