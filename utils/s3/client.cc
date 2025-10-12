06480d086751fe77690966dee3bff482898e94c0
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-10-12 17:51:33 +0300

    s3_client: unify `make_request` implementation
    Refactor `make_request` to use a single core implementation that
    handles authentication and issues the HTTP request. All overloads now
    delegate to this unified method.

--- utils/s3/client.cc
+++ utils/s3/client.cc
@@ -307,17 +307,15 @@ client::group_client& client::find_or_create_client() {
 
 future<> client::make_request(http::request req, http::experimental::client::reply_handler handle, std::optional<http::reply::status_type> expected, seastar::abort_source* as) {
     co_await authorize(req);
-    auto& gc = find_or_create_client();
-    co_await gc.retryable_client.make_request(std::move(req), std::move(handle), expected, as);
+    co_await find_or_create_client().retryable_client.make_request(std::move(req), std::move(handle), expected, as);
 }
 
 future<> client::make_request(http::request req, reply_handler_ext handle_ex, std::optional<http::reply::status_type> expected, seastar::abort_source* as) {
-    co_await authorize(req);
     auto& gc = find_or_create_client();
-    auto handle = [&gc, handle = std::move(handle_ex)] (const http::reply& rep, input_stream<char>&& in) {
+    auto handle = [&gc, handle = std::move(handle_ex)](const http::reply& rep, input_stream<char>&& in) {
         return handle(gc, rep, std::move(in));
     };
-    co_await gc.retryable_client.make_request(std::move(req), std::move(handle), expected, as);
+    co_await make_request(std::move(req), std::move(handle), expected, as);
 }
 
 future<> client::get_object_header(sstring object_name, http::experimental::client::reply_handler handler, seastar::abort_source* as) {
