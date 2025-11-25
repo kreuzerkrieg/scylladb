d3a00055d0a360f7a79dd00dfc9c36801eb341d4
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-11-25 16:26:43 +0200

    streaming: refactor tablet_sstable_streamer::stream by extracting SST filtering logic
    Extract the SST filtering logic into a dedicated member function. This
    prepares the code for independent testing without requiring the entire
    streamer to be initialized.

--- sstables_loader.hh
+++ sstables_loader.hh
@@ -10,6 +10,8 @@
 
 #include <vector>
 #include <seastar/core/sharded.hh>
+#include "dht/i_partitioner_fwd.hh"
+#include "dht/token.hh"
 #include "schema/schema_fwd.hh"
 #include "sstables/shared_sstable.hh"
 #include "tasks/task_manager.hh"
@@ -133,4 +135,10 @@ public:
             sstring endpoint, sstring bucket, stream_scope scope, bool primary_replica);
 
     class download_task_impl;
+};
+
+struct tablet_sstable_collection {
+    dht::token_range tablet_range;
+    std::vector<sstables::shared_sstable> sstables_fully_contained;
+    std::vector<sstables::shared_sstable> sstables_partially_contained;
 };
